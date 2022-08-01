57cbf1b5ff9ab63e311cc738d02f2690628018b2
Luis Andrade Reuss <reysh_2@yahoo.com>
2022-08-02 11:32:35 +1200

    Migrated

--- api/models.py
+++ api/models.py
@@ -1,6 +1,4 @@
-from enum import unique
-from tkinter import CASCADE
-from turtle import title
+
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
