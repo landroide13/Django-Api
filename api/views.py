9345174a08371b85eee3a2e7f1886ececb3cd2bf
Luis Andrade Reuss <reysh_2@yahoo.com>
2022-08-03 13:10:56 +1200

    is Authenticated

--- api/views.py
+++ api/views.py
@@ -4,13 +4,22 @@ from rest_framework.response import Response
 from rest_framework.decorators import action
 from django.contrib.auth.models import User
 from .models import Movie, Rating
-from .serializers import MovieSerializer, RatingSerializer
+from .serializers import MovieSerializer, RatingSerializer, UserSerializer
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 # Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+    authentication_classes = (TokenAuthentication, )
+    #permission_classes = (IsAuthenticated,)
+    permission_classes = (AllowAny,)
 
     @action(detail=True, methods=['POST'])
     def rate_movie(self, request, pk=None):
@@ -19,8 +28,8 @@ class MovieViewSet(viewsets.ModelViewSet):
             movie = Movie.objects.get(id=pk)
 
             stars = request.data['stars']
-            #user = request.user
-            user = User.objects.get(id=1)
+            user = request.user
+            #user = User.objects.get(id=1)
 
             try:
                 rating = Rating.objects.get(user=user.id, movie=movie.id)
@@ -49,7 +58,8 @@ class MovieViewSet(viewsets.ModelViewSet):
 class RatingViewSet(viewsets.ModelViewSet):
     queryset = Rating.objects.all()
     serializer_class = RatingSerializer
-
+    authentication_classes = (TokenAuthentication, )
+    permission_classes = (IsAuthenticated,)
 
 
 
--- rater/settings.py
+++ rater/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'api',
 ]
 
@@ -69,6 +70,12 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES':{
+        'rest_framework.permissions.IsAuthenticated'
     }
 }
 
